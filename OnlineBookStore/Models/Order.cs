b6d54fb4e43258572291ef553ce1a08227e2eb2d
Digo1703 <150072018+Digo1703@users.noreply.github.com>
2025-01-07 23:07:10 +0000

    Update Order.cs
    Added requirements on Order.cs

--- OnlineBookStore/Models/Order.cs
+++ OnlineBookStore/Models/Order.cs
@@ -1,17 +1,37 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OnlineBookStore.Models
 {
     public class Order
     {
         [Key]
+        [Column(TypeName = "int")]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "Deve conter no máximo 50 caracteres"), MinLength(3, ErrorMessage = "Deve conter no mínimo 3 caracteres")]
+        [Column(TypeName = "int")]
         public string BookId { get; set; }
         public Book book { get; set; } //Foreign Key
-        public string Username { get; set; }
-        public string Address { get; set; }
-        public decimal Price { get; set; }
-        public OrderStatus Status { get; set; } //Foreign Key
 
+        [Required]
+        [MaxLength(100, ErrorMessage = "Deve conter no máximo 100 caracteres"), MinLength(5, ErrorMessage = "Deve conter no mínimo 5 caracteres")]
+        [Column(TypeName = "varchar(100)")]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(350, ErrorMessage = "Deve conter no máximo 350 caracteres"), MinLength(50, ErrorMessage = "Deve conter no mínimo 50 caracteres")]
+        [Column(TypeName = "varchar(350)")]
+        public string Address { get; set; }
+
+        [Required]
+        [MaxLength(10, ErrorMessage = "Deve conter no máximo 100 caracteres"), MinLength(3, ErrorMessage = "Deve conter no mínimo 3 caracteres")]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Column(TypeName = "bit")]
+        public bool Status { get; set; } = false;
     }
 }
