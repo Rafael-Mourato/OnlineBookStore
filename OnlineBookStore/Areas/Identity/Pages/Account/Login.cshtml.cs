5389b668af29bef819b136c6bde25173e2720e71
Rafael-Mourato <rafaelmourato23@gmail.com>
2025-01-18 15:25:46 +0000

    Revamp login page and navbar with Bootstrap and localization
    The login page has been redesigned with a modern card layout using Bootstrap, and the content has been translated to Portuguese. The title has been changed to "Entrar em BookAlegre," and the layout is set to null. The "Remember me?" label is now "Lembrar," and the error message for invalid login attempts is "Credenciais Inválidas!" The `_LoginPartial.cshtml` file now includes a dropdown menu for user profile management and styled buttons for login and registration. Overall, the design has been modernized for a more cohesive and visually appealing user experience.

--- OnlineBookStore/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ OnlineBookStore/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -80,7 +80,7 @@ namespace OnlineBookStore.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Lembrar")]
             public bool RememberMe { get; set; }
         }
 
@@ -128,7 +128,7 @@ namespace OnlineBookStore.Areas.Identity.Pages.Account
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    ModelState.AddModelError(string.Empty, "Credenciais Inválidas!");
                     return Page();
                 }
             }
